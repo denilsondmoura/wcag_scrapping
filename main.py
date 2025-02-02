2521399d9a68a57821f3f3fa06bc6cabcacff800
Denilson Mendes de Moura <denilson.moura@interpi.pi.gov.br>
2025-02-02 19:04:08 -0300

    versão 1 - apenas verificação de relevancia

---
+++ main.py
@@ -0,0 +1,107 @@
+import requests
+import re
+import time
+from bs4 import BeautifulSoup
+
+wcag_url = 'https://www.w3.org/TR/WCAG22/'
+
+def get_page(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        return BeautifulSoup(response.text, 'html.parser')
+    else:
+        print(f'Page "{url}" not found: {response}')
+        
+def get_conformance_level(guideline):
+    conformance_level = guideline.find('p', {'class': 'conformance-level'})
+    if conformance_level:
+        return re.sub(r'\(Level\s*|\)', '', conformance_level.text)
+    else:
+        return ""
+    
+def get_secno(guideline):
+    secno = guideline.find('bdi', {'class': 'secno'})
+    if secno:
+        return re.sub(r'\(Guideline\s*|\)', '', secno.text)
+    else:
+        return ""
+    
+def get_principle_title(guideline):
+    secno = guideline.find('h2')
+    if secno:
+        return  secno.text
+    else:
+        return ""
+
+def get_guideline_title(guideline):
+    title_h3 = guideline.find('h3')
+    if title_h3:
+        return  title_h3.text
+    else:
+        title_h4 = guideline.find('h4')
+        
+        if title_h4:
+            return  title_h4.text
+        else:
+            return f'{get_secno(guideline)}{guideline.get("id")}' 
+    
+def is_guideline_relevant(guideline):
+    doclinks = guideline.find('div', {'class': 'doclinks'})
+    links = doclinks.find_all('a')
+    
+    for link in links:
+        if 'Understanding' in link.text:
+            understanding_page = get_page(link.get('href'))
+            
+            benefits = understanding_page.find('section', {'id': 'benefits'})
+            if benefits:
+                benefits_li = benefits.find_all('li')
+                
+                for li in benefits_li:
+                    if 'low vision' in li.text or 'blind' in li.text:
+                        return True
+                    
+            else:
+                return False
+        else:
+            return False
+ 
+def is_success_criterion(title):
+    if 'Success Criteri' in title:
+        return True
+    else:
+        return False
+
+ 
+success_criterion_count = 1
+wcag_page = get_page(wcag_url)   
+
+if wcag_page:
+    principles = wcag_page.find_all('section', {'class': 'principle'})
+
+    for principle in principles:
+        principle_title = get_principle_title(principle)
+        print(f'{principle_title}')
+        
+        guidelines = principle.find_all('section', {'class': 'guideline'})
+        for guideline in guidelines:
+                    
+            conformance_level = get_conformance_level(guideline)
+            guideline_title = get_guideline_title(guideline)
+            is_relevant = is_guideline_relevant(guideline)
+            
+            if not is_success_criterion(guideline_title) or is_relevant:
+                print(f'{success_criterion_count} - {guideline_title} ({conformance_level})')
+                
+            if is_relevant:
+                success_criterion_count+=1
+                
+            time.sleep(2)
+
+    print(f'Total uccess Criterion', success_criterion_count)
+
